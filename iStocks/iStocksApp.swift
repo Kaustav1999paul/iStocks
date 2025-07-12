58003dd6d07b0884f40e350a20e922b43eae12c0
kaustav <kaustavpaul347@gmail.com>
2025-07-12 22:13:26 +0530

    Initial Commit

---
+++ iStocks/iStocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  iStocksApp.swift
+//  iStocks
+//
+//  Created by Kaustav Paul on 12/07/25.
+//
+
+import SwiftUI
+
+@main
+struct iStocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
