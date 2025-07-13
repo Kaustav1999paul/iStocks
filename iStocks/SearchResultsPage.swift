65f229ab332bd25e1ccacbd2a584db08615638fd
kaustav <kaustavpaul347@gmail.com>
2025-07-13 13:23:26 +0530

    iStock App

---
+++ iStocks/SearchResultsPage.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResultsPage.swift
+//  iStocks
+//
+//  Created by Kaustav Paul on 13/07/25.
+//
+
+import SwiftUI
+
+struct SearchResultsPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchResultsPage()
+}
